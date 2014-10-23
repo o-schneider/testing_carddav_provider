660b978123b60707fd64ccd2e8f5a62ce02ddc43
tuxgasy <contact@tuxgasy.fr>
2014-10-24 01:41:31 +0200

    use Contacts API

--- js/app.js
+++ js/app.js
@@ -8,8 +8,6 @@ var App = function App() {
   var info = null;
   var fillButton, resetButton;
 
-  var DS_NAME = 'contacts';
-
   var init = function init() {
     info = document.getElementById('info');
     fillButton = document.getElementById('fillDS');
@@ -17,8 +15,6 @@ var App = function App() {
 
     fillButton.addEventListener('click', handleEvent);
     resetButton.addEventListener('click', handleEvent);
-
-    initDS();
   };
 
   function handleEvent(evt) {
@@ -31,25 +27,75 @@ var App = function App() {
 
     switch (btn) {
       case 'fillDS':
-      fillButton.disabled = true;
-      DAVLib.initLogin(store, accountData, function() {
-        DAVLib.importContacts(function() {
-          fillButton.disabled = false;
-          store.getLength().then(function(count) {
-            info.textContent = count + ' elements';
+        fillButton.disabled = true;
+
+        var openedDAVConnection = jsDAVlib.getConnection(accountData);
+        openedDAVConnection.onready = function() {
+          console.log('connectionInfo: ' +
+            JSON.stringify(openedDAVConnection.getInfo()));
+
+          openedDAVConnection.getResource(null, function(res, error) {
+            if (error) {
+              console.log('Error getting main resource - ' + error);
+              return;
+            }
+
+            var openedDAVMainResource = res;
+
+            var contactsList = openedDAVMainResource.get().data;
+            var contactsResource = [];
+            for (var i = 0; i < contactsList.length; i++) {
+              openedDAVConnection.getResource(contactsList[i].href, function(res, e) {
+                var vcard = res.contents.split('\r\n');
+                var contact = new mozContact();
+                for (var j = 0; j < vcard.length; j++) {
+                  if (/^N:/.test(vcard[j])) {
+                    var name = vcard[j].split(':')[1].split(';');
+                    contact.familyName = [name[0]];
+                    contact.givenName = [name[1]];
+                    contact.additionalName = [name[2]];
+                    contact.honorificPrefix = [name[3]];
+                    contact.honorificSuffix = [name[4]];
+                  } else if (/^FN:/.test(vcard[j])) {
+                    var name = vcard[j].split(':');
+                    contact.name = [name[1]];
+                  } else if (/^TEL;/.test(vcard[j])) {
+                    var allTel = vcard[j].split(';');
+                    var tel = allTel[1].split(':');
+                    contact.tel = [{
+                      type: [tel[0].split('=')[1]],
+                      value: tel[1]
+                    }];
+                  }
+                }
+
+                var saving = navigator.mozContacts.save(contact);
+                saving.onsuccess = function() {
+                  console.log('new contact saved');
+                };
+                saving.onerror = function(err) {
+                  console.error(err);
+                };
+              });
+            }
           });
-        });
-      });
-      break;
+        };
+
+        break;
       case 'resetDS':
-      store.clear().then(function() {
-        store.getLength().then(function(count) {
-          info.textContent = count + ' elements';
-        });
-      });
-      break;
+        var q = confirm("Remove ALL the contacts from ? Be careful!!! There is no way back!");
+        if (q) {
+          var req = navigator.mozContacts.clear();
+          req.onsuccess = function() {
+            alert('All contacts have been removed.');
+          };
+          req.onerror = function() {
+            alert('[error] No contacts were removed.');
+          };
+        }
+        break;
       default:
-      break;
+        break;
     }
   }
 
